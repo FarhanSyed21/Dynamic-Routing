917bce7c992399e17a50777fe900a87264e73e48
Syed Farhan <farhansyed0007@gmail.com>
2023-08-20 13:56:23 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,5 @@
 import MeetupList from '../components/meetups/MeetupList' 
-
-const DUMMY_MEETUPS = [
-    {   id: "m1",
-        title: "First Meet",
-        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/800px-Stadtbild_M%C3%BCnchen.jpg?20130611211153",
-        address: "some random street, some landmark, random appartment, some floor, some flat number",
-        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-    },
-    {   id: "m2",
-        title: "Second Meet",
-        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/800px-Stadtbild_M%C3%BCnchen.jpg?20130611211153",
-        address: "some random street, some landmark, random appartment, some floor, some flat number",
-        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-    },
-    {   id: "m3",
-        title: "Third Meet",
-        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/800px-Stadtbild_M%C3%BCnchen.jpg?20130611211153",
-        address: "some random street, some landmark, random appartment, some floor, some flat number",
-        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-    },
-    {   id: "m4",
-        title: "Fourth Meet",
-        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/800px-Stadtbild_M%C3%BCnchen.jpg?20130611211153",
-        address: "some random street, some landmark, random appartment, some floor, some flat number",
-        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-    },
-]
+import { MongoClient } from 'mongodb'
 
 const index = (props) => {
   return (
@@ -45,9 +19,23 @@ const index = (props) => {
 // }
 
 export async function getStaticProps(){
+    const client = await MongoClient.connect('mongodb+srv://farhansyed0007:farhan12112@cluster0.btjikoo.mongodb.net/meetups?retryWrites=true&w=majority');
+        const db = client.db();
+
+        const meetupsCollection = db.collection('meetups');
+
+        const meetups = await meetupsCollection.find().toArray();
+
+        client.close();
+
     return{
         props: {
-            meetups: DUMMY_MEETUPS
+            meetups: meetups.map(meetups => ({
+                title: meetups.title,
+                address: meetups.address,
+                image: meetups.image,
+                id: meetups._id.toString(),
+            }))
         },
     }
 }
